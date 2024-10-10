0ef416a685e7af7a03422ffe73c5a2fe99f558de
GoXs05 <gokulsriram992005@gmail.com>
2024-10-10 16:08:46 -0500

    Navbar Shrinking & Background

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-<script>
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
     });
-</script>
+});
+
+// Shrinking Navbar on Scroll
+window.onscroll = function() {
+    const navbar = document.getElementById("navbar");
+    if (window.pageYOffset > 50) {
+        navbar.classList.add("shrink");
+    } else {
+        navbar.classList.remove("shrink");
+    }
+};
